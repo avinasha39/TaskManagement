37a609e1570d9787c6526bd1f51612a4c1e873c9
Avinash Agarwal <avinasha39@gmail.com>
2021-05-02 16:47:39 +0530

    Removing frontend code

--- projectTaskFrontend/angularUi/src/app/models/project.ts
+++
@@ -1,7 +0,0 @@
-export interface Project {
-  id:number;
-  title : string;
-  description : string;
-  duration : string;
-  photo: string;
-}
--- projectTaskFrontend/angularUi/src/app/models/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-  id : number;
-  title:string;
-  description:string;
-  startDate:Date;
-  endDate:Date;
-  project:number;
-}
